f26e055cf4137149edda3ab770e68becf08d701f
bbeaird <bbeaird@gmail.com>
2013-11-04 17:34:06 -0800

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,8 @@ class User < ActiveRecord::Base
   has_many :skills, :through => :appointments
 
   def proficiency_for(skill)
-    # p self.appointments#[0].proficiency
-    # p self.skills.find_by_name("Beard Growing")#[0].proficiency
-    # p self.skills
-    # p user = self
-    # self.skill
-
-    # if self.appointments[0].proficiency.nil?
-    #   self.appointments[0].proficiency = 0
-    # end
-
-    # p self.appointments.find_by_skill_id(19)
-    # p Skill.all
-    # p Skill.find_by_name("Beard Growing")
     p self.appointments
-    p skill_id = self.skills.find_by_name(skill.name).id#.find_by_name(skill)
+    p skill_id = self.skills.find_by_name(skill.name).id
     p self.appointments.find_by_skill_id(skill_id).proficiency
   end
 
